dc432c74dd4a36caa4228a8c0f14405f298fe5d2
Csaba Paronai <paronai.csaba@gmail.com>
2025-04-04 16:23:41 +0100

    Export app

--- src/server.ts
+++ src/server.ts
@@ -50,8 +50,8 @@ app.get("/logs", async (_: Request, res: Response) => {
 });
 
 // Start server
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port " + process.env.PORT);
 });
 
 export default app;
